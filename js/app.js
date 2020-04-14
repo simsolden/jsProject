d8781ec5c2c329a63e26c256c8af4c088a20dd08
Simon Oldenhove <simonoldenhove@gmail.com>
2020-04-14 23:34:52 +0200

    First js function: GET wines with ajax request

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 //Variables
 
 //Functions
-
+$( document ).ready(function() {
+	let $wines = $("#wines");
+	$.ajax({
+		type: 'GET',
+		url:"js/api/wines",
+		dataType: 'JSON',
+		success:function(wines){
+			$.each(wines, function(i, wine){
+				$wines.append("<buttonid=\"buttonWine"+wine.id+"\" name=\"buttonWine"+wine.id+"+\" type=\"button\" class=\"btn btn-outline-secondary btn-block\">"+wine.name+"</button>");
+			});
+		},
+		error:function(){
+			alert("Ajax error retrieving Wine JSON");
+		}
+	});
+});
 //Main
